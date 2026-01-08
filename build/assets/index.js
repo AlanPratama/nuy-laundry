3f8af9772351a5cc0932f91764744a5013eddb98
AlanPratama <pralan76@gmail.com>
2026-01-08 20:30:33 +0700

    init deploy

---
+++ build/assets/index.js
@@ -0,0 +1,13 @@
+import hero from "./hero.png"
+import imageService1 from "./imageService1.png"
+import imageService2 from "./imageService2.png"
+import imageService3 from "./imageService3.png"
+import washingMachine from "./washing-machine-icon.svg"
+
+export const images = {
+    hero,
+    imageService1,
+    imageService2,
+    imageService3,
+    washingMachine
+}
