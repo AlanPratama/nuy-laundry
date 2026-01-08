d1a6020369e00fb0aa3e238bd9bd02499e31ddad
AlanPratama <pralan76@gmail.com>
2026-01-08 20:52:49 +0700

    Updates

---
+++ index.js
@@ -0,0 +1,13 @@
+import hero from "./assets/hero.png"
+import imageService1 from "./assets/imageService1.png"
+import imageService2 from "./assets/imageService2.png"
+import imageService3 from "./assets/imageService3.png"
+import washingMachine from "./assets/washing-machine-icon.svg"
+
+export const images = {
+    hero,
+    imageService1,
+    imageService2,
+    imageService3,
+    washingMachine
+}
